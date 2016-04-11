71f2b7db630c9a40e5b27ba72727be9cf95e077c
15692186320 <785625710@qq.com>
2016-04-12 07:46:20 +0800

    Initial Commit

---
+++ Bank/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bank
+//
+//  Created by 朱元恺 on 16/4/12.
+//  Copyright © 2016年 朱元恺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bank
+//
+//  Created by 朱元恺 on 16/4/12.
+//  Copyright © 2016年 朱元恺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
